36ce3a2d310ffb34eb1f53ee38ef61025aa8d89e
Natanael Copa <ncopa@alpinelinux.org>
2023-01-20 12:06:54 +0100

    lddtree: fix shellcheck linting warnings and errors

--- lddtree.sh
+++ lddtree.sh
@@ -7,7 +7,7 @@
 argv0=${0##*/}
 version=1.26
 
-: ${ROOT:=/}
+: "${ROOT:=/}"
 
 [ "${ROOT}" = "${ROOT%/}" ] && ROOT="${ROOT}/"
 [ "${ROOT}" = "${ROOT#/}" ] && ROOT="${PWD}/${ROOT}"
@@ -24,14 +24,17 @@ error() {
 
 # functions for scanelf backend
 elf_rpath_scanelf() {
+	# shellcheck disable=SC2317
 	scanelf -qF '#F%r' "$@"
 }
 
 elf_interp_scanelf() {
+	# shellcheck disable=SC2317
 	scanelf -qF '#F%i' "$@"
 }
 
 elf_needed_scanelf() {
+	# shellcheck disable=SC2317
 	scanelf -qF '#F%n' "$@"
 }
 
@@ -44,12 +47,14 @@ elf_specs_scanelf() {
 	# With glibc, the NONE, SYSV, GNU, and LINUX OSABI's are compatible.
 	# LINUX and GNU are the same thing, as are NONE and SYSV, so normalize
 	# GNU & LINUX to NONE. #442024 #464380
+	# shellcheck disable=SC2317
 	scanelf -BF '#F%a %M %D %I' "$1" | \
 		sed -r 's: (LINUX|GNU)$: NONE:'
 }
 
 # functions for binutils backend
 elf_rpath_binutils() {
+	# shellcheck disable=SC2317
 	objdump -x "$@" | awk '$1 == "RUNPATH" { if($2!="") {runpath=$2;} }  $1 == "RPATH" { if($2!="") {rpath=$2;} } END { if(runpath!="") {print runpath;} else {print rpath;} }'
 }
 
@@ -59,16 +64,19 @@ elf_interp_binutils() {
 	# String dump of section '.interp':
 	#  [     0]  /lib/ld-musl-x86_64.so.1
 	#
+	# shellcheck disable=SC2317
 	readelf  -p .interp "$1" | sed -E -n '/\[\s*[0-9]\]/s/^\s*\[.*\]\s*(.*)/\1/p'
 }
 
 elf_needed_binutils() {
+	# shellcheck disable=SC2317
 	objdump -x "$@" | awk '$1 == "NEEDED" { line=line sep $2 ; sep="," } END { print line }'
 }
 
 elf_specs_binutils() {
 	# get Class, Data, Machine and OS/ABI.
 	# the OS/ABI 'GNU', 'System V' and 'Linux' are compatible so normalize
+	# shellcheck disable=SC2317
 	readelf -h "$1" \
 		| awk -F: '$1 ~ /Class|Data|Machine|OS.ABI/ {gsub(/^ +/, "", $2); print $2}' \
 		| sed -E -e 's/UNIX - (System V|Linux|GNU)/UNIX/' \
@@ -82,7 +90,7 @@ elf_needed() { elf_needed_$BACKEND "$@"; }
 elf_specs() { elf_specs_$BACKEND "$1"; }
 
 unset lib_paths_fallback
-for p in ${ROOT}lib* ${ROOT}usr/lib* ${ROOT}usr/local/lib*; do
+for p in "${ROOT}"lib* "${ROOT}"usr/lib* "${ROOT}"usr/local/lib*; do
 	lib_paths_fallback="${lib_paths_fallback}${lib_paths_fallback:+:}${p}"
 done
 c_ldso_paths_loaded='false'
@@ -98,12 +106,13 @@ find_elf() {
 			local elf="$1"
 			local pathstr="$2"
 			IFS=:
+			# shellcheck disable=SC2086
 			set -- $pathstr
 			unset IFS
 			local path pe
 			for path ; do
-				: ${path:=${PWD}}
-				if [ "${path#${ROOT}}" = "${path}" ]; then
+				: "${path:=${PWD}}"
+				if [ "${path#"${ROOT}"}" = "${path}" ]; then
 					path="${ROOT}${path#/}"
 				fi
 				pe="${path%/}/${elf#/}"
@@ -139,10 +148,10 @@ find_elf() {
 					# if the glob didnt match anything #360041,
 					# or the files arent readable, skip it
 					[ -r "${p}" ] || continue
-					while read line ; do
+					while read -r line ; do
 						case ${line} in
 							"#"*) ;;
-							"include "*) read_ldso_conf ${line#* } ;;
+							"include "*) read_ldso_conf "${line#* }" ;;
 							*) c_ldso_paths="$c_ldso_paths:${ROOT}${line#/}";;
 						esac
 					done <"${p}"
@@ -162,7 +171,7 @@ find_elf() {
 				if [ -r "$ldso_conf" ]; then
 					read_ldso_conf "$ldso_conf"
 				fi
-				cd "$_oldpwd"
+				cd "$_oldpwd" || return
 			fi
 		fi
 		if [ -n "${c_ldso_paths}" ] ; then
@@ -189,20 +198,20 @@ resolv_links() {
 	_list_files="$2"
 	local oldpwd="$PWD"
 	[ "$_list_files" = yes ] && list_existing_file "${_resolv_links}"
-	cd "${_resolv_links%/*}"
+	cd "${_resolv_links%/*}" || return
 	while [ -L "$_resolv_links" ]; do
 		_resolv_links=$(readlink "$_resolv_links")
 		case "$_resolv_links" in
 		/*)	_resolv_links="${ROOT}${_resolv_links#/}"
-			cd "${_resolv_links%/*}"
+			cd "${_resolv_links%/*}" || break
 			;;
-		*/*)	cd "${_resolv_links%/*}"
+		*/*)	cd "${_resolv_links%/*}" || break
 			;;
 		esac
 		_resolv_links=$(pwd -P)/${_resolv_links##*/}
 		[ "$_list_files" = yes ] && list_existing_file "${_resolv_links}"
 	done
-	cd "$oldpwd"
+	cd "$oldpwd" || return
 }
 
 show_elf() {
@@ -216,7 +225,7 @@ show_elf() {
 	${LIST} || printf "%${indent}s%s => " "" "${elf}"
 	case ",${parent_elfs}," in
 	*,${elf},*)
-		${LIST} || printf "!!! circular loop !!!\n" ""
+		${LIST} || printf "!!! circular loop !!!\n"
 		return
 		;;
 	esac
@@ -225,10 +234,10 @@ show_elf() {
 		resolv_links "${resolved:-$1}" yes
 	else
 		resolv_links "${resolved:-$1}" no
-		printf "${resolved:-not found}"
+		printf "%s" "${resolved:-not found}"
 	fi
 	resolved=${_resolv_links}
-	if [ ${indent:-0} -eq 0 ] ; then
+	if [ "${indent:-0}" -eq 0 ] ; then
 		elf_specs=$(elf_specs "${resolved}")
 		interp=$(elf_interp "${resolved}")
 		# ignore interpreters that do not have absolute path
@@ -238,7 +247,7 @@ show_elf() {
 		if ${LIST} ; then
 			[ -n "${interp}" ] && resolv_links "${interp}" yes
 		else
-			printf " (interpreter => ${interp:-none})"
+			printf "%s" " (interpreter => ${interp:-none})"
 		fi
 		if [ -r "${interp}" ] ; then
 			# Extract the default lib paths out of the ldso.
@@ -264,6 +273,7 @@ show_elf() {
 
 	oifs="$IFS"
 	IFS=,
+	# shellcheck disable=SC2086
 	set -- ${libs}
 	IFS="$oifs"
 
@@ -300,8 +310,7 @@ SET_X=false
 LIST=false
 AUTO_ROOT=true
 
-OPTS=$(getopt -n "$argv0" -o "ahlR:Vx" -l "all,help,list,no-auto-root,root:,version,debug" -- "$@")
-if [ $? -ne 0 ]; then
+if ! OPTS=$(getopt -n "$argv0" -o "ahlR:Vx" -l "all,help,list,no-auto-root,root:,version,debug" -- "$@"); then
 	usage >&2
 	exit 1
 fi
@@ -349,7 +358,7 @@ for elf ; do
 		error "${elf}: file is not readable"
 	elif [ -d "${elf}" ] ; then
 		if $LIST; then
-			echo ${elf}
+			echo "${elf}"
 		else
 			error "${elf}: is a directory"
 		fi
