9501778ecfae64d70e8b737eacc4fa4521a80e65
Natanael Copa <ncopa@alpinelinux.org>
2023-01-23 13:45:10 +0100

    lddtree: handle direct ldso linkage better
    If the ELF we're showing is linked directly against the ELF which is used
    as the ELF interp (i.e. the ldso), make sure we don't list it twice.
    
    upstream: https://github.com/gentoo/pax-utils/commit/26ee546756ce9e1bb84951bc5cfac3cdde87a2d1

--- lddtree.sh
+++ lddtree.sh
@@ -258,6 +258,11 @@ show_elf() {
 			)
 		fi
 		interp=${interp##*/}
+		# If we are in non-list mode, then we want to show the "duplicate" interp
+		# lines -- first the header (interp=>xxx), and then the DT_NEEDED line to
+		# show that the ELF is directly linked against the interp.
+		# If we're in list mode though, we only want to show the interp once.
+		${LIST} && allhits="${allhits},${interp}"
 	fi
 	${LIST} || printf "\n"
 
@@ -268,7 +273,7 @@ show_elf() {
 	local my_allhits
 	if ! ${SHOW_ALL} ; then
 		my_allhits="${allhits}"
-		allhits="${allhits},${interp},${libs}"
+		allhits="${allhits},${libs}"
 	fi
 
 	oifs="$IFS"
@@ -279,7 +284,9 @@ show_elf() {
 
 	for lib; do
 		lib=${lib##*/}
-		case ",${my_allhits}," in
+		# No need for leading comma w/my_allhits as we guarantee it always
+		# starts with one due to the way we append the value above.
+		case "${my_allhits}," in
 			*,${lib},*) continue;;
 		esac
 		find_elf "${lib}" "${resolved}"
